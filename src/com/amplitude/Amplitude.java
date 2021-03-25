849f11ab7d8bfadffc657a64c99dfbaf4e644e8f
Dante Tam <dante.tam1@gmail.com>
2021-03-25 12:55:09 -0700

    Basic Java API

---
+++ src/com/amplitude/Amplitude.java
@@ -0,0 +1,29 @@
+package com.amplitude;
+
+import java.util.Map;
+
+public class Amplitude {
+
+    private static Map<String, Amplitude> instances;
+
+    public static Amplitude getInstance(String instanceName) {
+        return null;
+    }
+
+    public void init(String apiKey) {
+
+    }
+
+    public void logEvent(String name) {
+
+    }
+
+    public void logEvent(String name, Event event) {
+
+    }
+
+    public void setUserProperties(JSONObject properties) {
+
+    }
+
+}
---
+++ src/com/amplitude/Event.java
@@ -0,0 +1,9 @@
+package com.amplitude;
+
+import org.json.simple.JSONObject;
+
+public class Event {
+
+    private JsonObject eventProps;
+
+}
