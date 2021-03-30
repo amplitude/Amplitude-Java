d2d283f6ae09708de57044e8d807e7e85bf2a941
Dante Tam <dante.tam1@gmail.com>
2021-03-30 12:03:25 -0700

    Write out basic API

--- src/com/amplitude/Amplitude.java
+++ src/com/amplitude/Amplitude.java
@@ -5,25 +5,38 @@ import java.util.Map;
 public class Amplitude {
 
     private static Map<String, Amplitude> instances;
+    private JSONObject userProperties;
+    private String apiKey;
 
     public static Amplitude getInstance(String instanceName) {
-        return null;
+        if (!instances.containsKey(instanceName)) {
+            Amplitude ampInstance = new Amplitude();
+            newAmpInstance.put(instanceName, ampInstance)
+        }
+        else {
+            return instances.get(instanceName);
+        }
     }
 
-    public void init(String apiKey) {
-
+    public void init(String key) {
+        apiKey = key;
     }
 
     public void logEvent(String name) {
-
+        logEvent(name, null);
     }
 
     public void logEvent(String name, Event event) {
-
+        //Use HTTPUrlConnection object to make async HTTP request,
+        //using data from event like device, class name, event props, etc.
     }
 
-    public void setUserProperties(JSONObject properties) {
+    public JSONObject getUserProperties() {
+        return userProperties;
+    }
 
+    public void setUserProperties(JSONObject userProperties) {
+        this.userProperties = userProperties;
     }
 
 }
