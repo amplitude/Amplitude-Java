df00d248d9d1b95411585d0ebc7bdf4dfd78b3a3
yuhao900914 <yuhao900914@gmail.com>
2021-04-13 15:11:07 -0700

    log event in controller

--- src/demo/java/com/amplitude/Demo.java
+++ src/demo/java/com/amplitude/Demo.java
@@ -8,15 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
-import com.amplitude.Amplitude;
-
 @SpringBootApplication
 public class Demo {
 	public static void main(String[] args) {
-		System.out.println("Amplitude Java Demo:");
-		Amplitude amplitude = Amplitude.getInstance("INSTANCE_NAME");
-		amplitude.init("bcfd68add3ac9532f2f0920bdee7f9e2");
-		amplitude.logEvent("Event name test");
+		System.out.println("Running Amplitude Java Demo");
 		SpringApplication.run(Demo.class, args);
 	}
 }
--- src/demo/java/com/amplitude/DemoController.java
+++ src/demo/java/com/amplitude/DemoController.java
@@ -3,10 +3,14 @@ package com.amplitude;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.amplitude.Amplitude;
 @RestController
 public class DemoController {
 	@RequestMapping("/")
 	public String index() {
-		return "Amplitude Java SDK Demo!";
+		Amplitude amplitude = Amplitude.getInstance("INSTANCE_NAME");
+		amplitude.init("bcfd68add3ac9532f2f0920bdee7f9e2");
+		amplitude.logEvent("Event name test");
+		return "Amplitude Java SDK Demo: sending test event.";
 	}
 }
