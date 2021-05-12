b98b96eca7fb913808a547773aa8c9ac6b778229
Dante Tam <dante.tam1@gmail.com>
2021-05-12 10:22:42 -0700

    Clean up project specs and compile for Java 8 target

--- src/main/java/com/amplitude/Constants.java
+++ src/main/java/com/amplitude/Constants.java
@@ -5,7 +5,7 @@ public interface Constants {
     String  API_URL                 = "https://api2.amplitude.com/2/httpapi";
     long    NETWORK_TIMEOUT_MILLIS  = 10000;
     String  SDK_LIBRARY             = "amplitude-java";
-    String  SDK_VERSION             = "1.0.0";
+    String  SDK_VERSION             = "1.1.0";
 
     int     MAX_PROPERTY_KEYS       = 1024;
     int     MAX_STRING_LENGTH       = 1000;
