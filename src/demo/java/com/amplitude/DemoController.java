3c3bb17838e538d16316bcc56150a0dd32e3ee1e
Dante Tam <dante.tam1@gmail.com>
2021-05-17 13:42:06 -0700

    Revert demo changes

--- src/demo/java/com/amplitude/DemoController.java
+++ src/demo/java/com/amplitude/DemoController.java
@@ -1,6 +1,5 @@
 package com.amplitude;
 
-import org.json.JSONObject;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,17 +10,9 @@ public class DemoController {
 	@RequestMapping("/")
 	public String index() {
 		Amplitude amplitude = Amplitude.getInstance("INSTANCE_NAME");
-		amplitude.init("378fc916942fe9bee9d2dbd599508c6c");
+		amplitude.init("8e07b9d451a7d07bd33f6e9ba5870f21");
+		amplitude.logEvent(new Event("Test Event", "test_user_id"));
 		amplitude.setLogMode(AmplitudeLog.LogMode.DEBUG);
-
-		for (int i = 0; i < 15; i++) {
-			Event event = new Event("Button Clicked " + i, "test_user_for_demo");
-			event.eventProperties = new JSONObject()
-					.put("favorite_color", "red")
-					.put("special_data", new double[]{1, 2, 4, 8, 16});
-			amplitude.logEvent(event);
-		}
-
-		return "Amplitude Java SDK Demo: sending test event. " + System.currentTimeMillis();
+		return "Amplitude Java SDK Demo: sending test event.";
 	}
 }
