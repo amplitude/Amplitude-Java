3c49f5a566ccc7701e69ff009d845c616fcbe5d2
Qingzhuo Zhen <qingzhuo.zhen@amplitude.com>
2021-06-26 15:14:42 -0700

    fix: reformat code

--- src/test/java/com/amplitude/AmplitudeTest.java
+++ src/test/java/com/amplitude/AmplitudeTest.java
@@ -1,21 +1,20 @@
 package com.amplitude;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotSame;
 import static org.junit.jupiter.api.Assertions.assertSame;
 
 public class AmplitudeTest {
 
-    @Test
-    public void testGetInstance() {
-        Amplitude a = Amplitude.getInstance();
-        Amplitude b = Amplitude.getInstance("");
-        Amplitude d = Amplitude.getInstance("app1");
-        Amplitude e = Amplitude.getInstance("app2");
+  @Test
+  public void testGetInstance() {
+    Amplitude a = Amplitude.getInstance();
+    Amplitude b = Amplitude.getInstance("");
+    Amplitude d = Amplitude.getInstance("app1");
+    Amplitude e = Amplitude.getInstance("app2");
 
-        assertSame(a, b);
-        assertNotSame(d, e);
-        assertNotSame(a, d);
-    }
+    assertSame(a, b);
+    assertNotSame(d, e);
+    assertNotSame(a, d);
+  }
 }
