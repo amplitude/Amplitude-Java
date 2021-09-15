5986bbda67a555bb9ae5981d84fe1fd5e02cad50
Alyssa Yu <yuhao900914@gmail.com>
2021-09-14 20:00:14 -0700

    fix: add revenue props in the event payload when there has revenue or… (#30)
    * fix: add revenue props in the event payload when there has revenue or quantity and price info
    
    * add test for new changes on revenue properties

--- src/test/java/com/amplitude/EventTest.java
+++ src/test/java/com/amplitude/EventTest.java
@@ -1,6 +1,7 @@
 package com.amplitude;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.json.JSONArray;
@@ -50,5 +51,45 @@ public class EventTest {
     assertEquals(userId, truncatedEvent.getString("user_id"));
     assertEquals(
         Constants.SDK_LIBRARY + "/" + Constants.SDK_VERSION, truncatedEvent.getString("library"));
+  }
+
+  @Test
+  public void testLogEventWithRevenue() {
+    String eventType = "test event type";
+    String userId = "test-user";
+    Double price = 10.34;
+    int quantity = 1;
+    String productId = "test_product_id";
+    String revenueType = "test_revenue_type";
+    Event event = new Event(eventType, userId);
+    event.revenue = price * quantity;
+    event.price = price;
+    event.productId = productId;
+    event.revenueType = revenueType;
+
+    JSONObject truncatedEvent = event.toJsonObject();
+    assertEquals(eventType, truncatedEvent.getString("event_type"));
+    assertEquals(userId, truncatedEvent.getString("user_id"));
+    assertEquals(price * quantity, truncatedEvent.getDouble("revenue"));
+    assertEquals(price, truncatedEvent.getDouble("price"));
+    assertEquals(quantity, truncatedEvent.getInt("quantity"));
+    assertEquals(productId, truncatedEvent.getString("productId"));
+    assertEquals(revenueType, truncatedEvent.getString("revenueType"));
+  }
+
+  @Test
+  public void testLogEventWithoutRevenue() {
+    String eventType = "test event type";
+    String userId = "test-user";
+    Event event = new Event(eventType, userId);
+
+    JSONObject truncatedEvent = event.toJsonObject();
+    assertEquals(eventType, truncatedEvent.getString("event_type"));
+    assertEquals(userId, truncatedEvent.getString("user_id"));
+    assertFalse(truncatedEvent.has("revenue"));
+    assertFalse(truncatedEvent.has("price"));
+    assertFalse(truncatedEvent.has("quantity"));
+    assertFalse(truncatedEvent.has("productId"));
+    assertFalse(truncatedEvent.has("revenueType"));
   }
 }
