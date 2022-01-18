e51d5e4acce1f105661e5d22486b9b0fb7ce049b
bohan-amplitude <bohan.li@amplitude.com>
2022-01-18 13:24:31 -0800

    add test for failed response and unknown response that won't retry

--- src/test/java/com/amplitude/HttpTransportTest.java
+++ src/test/java/com/amplitude/HttpTransportTest.java
@@ -245,17 +245,12 @@ public class HttpTransportTest {
         Response timeoutResponse = ResponseUtil.getTimeoutResponse();
         Response successResponse = ResponseUtil.getSuccessResponse();
         HttpCall httpCall = mock(HttpCall.class);
-        CountDownLatch latch = new CountDownLatch(2);
+        CountDownLatch latch = new CountDownLatch(1);
         when(httpCall.makeRequest(anyList()))
         .thenAnswer(
                 invocation -> {
                     latch.countDown();
                     return timeoutResponse;
-                })
-        .thenAnswer(
-                invocation -> {
-                    latch.countDown();
-                    return successResponse;
                 });
 
         List<Event> events = EventsGenerator.generateEvents(10);
@@ -271,9 +266,73 @@ public class HttpTransportTest {
         httpTransport.setCallbacks(callbacks);
         httpTransport.retryEvents(events, timeoutResponse);
         assertTrue(latch.await(1L, TimeUnit.SECONDS));
-        verify(httpCall, times(2)).makeRequest(anyList());
+        verify(httpCall, times(1)).makeRequest(anyList());
         for (int i = 0; i < events.size(); i++) {
-            assertEquals(200, resultMap.get(events.get(i)));
+            assertEquals(408, resultMap.get(events.get(i)));
+        }
+    }
+
+    @Test
+    public void testFailedResponse()
+            throws AmplitudeInvalidAPIKeyException, InterruptedException {
+        Response failedResponse = ResponseUtil.getFailedResponse();
+        HttpCall httpCall = mock(HttpCall.class);
+        CountDownLatch latch = new CountDownLatch(1);
+        when(httpCall.makeRequest(anyList()))
+                .thenAnswer(
+                        invocation -> {
+                            latch.countDown();
+                            return failedResponse;
+                        });
+
+        List<Event> events = EventsGenerator.generateEvents(10);
+        Map<Event, Integer> resultMap = new HashMap<>();
+        AmplitudeCallbacks callbacks =
+                new AmplitudeCallbacks() {
+                    @Override
+                    public void onLogEventServerResponse(Event event, int status, String message) {
+                        resultMap.put(event, status);
+                    }
+                };
+        httpTransport.setHttpCall(httpCall);
+        httpTransport.setCallbacks(callbacks);
+        httpTransport.sendEventsWithRetry(events);
+        assertTrue(latch.await(1L, TimeUnit.SECONDS));
+        verify(httpCall, times(1)).makeRequest(anyList());
+        for (int i = 0; i < events.size(); i++) {
+            assertEquals(500, resultMap.get(events.get(i)));
+        }
+    }
+
+    @Test
+    public void testUnknownResponse()
+            throws AmplitudeInvalidAPIKeyException, InterruptedException {
+        Response unknownResponse = ResponseUtil.getUnknownResponse();
+        HttpCall httpCall = mock(HttpCall.class);
+        CountDownLatch latch = new CountDownLatch(1);
+        when(httpCall.makeRequest(anyList()))
+                .thenAnswer(
+                        invocation -> {
+                            latch.countDown();
+                            return unknownResponse;
+                        });
+
+        List<Event> events = EventsGenerator.generateEvents(10);
+        Map<Event, Integer> resultMap = new HashMap<>();
+        AmplitudeCallbacks callbacks =
+                new AmplitudeCallbacks() {
+                    @Override
+                    public void onLogEventServerResponse(Event event, int status, String message) {
+                        resultMap.put(event, status);
+                    }
+                };
+        httpTransport.setHttpCall(httpCall);
+        httpTransport.setCallbacks(callbacks);
+        httpTransport.sendEventsWithRetry(events);
+        assertTrue(latch.await(1L, TimeUnit.SECONDS));
+        verify(httpCall, times(1)).makeRequest(anyList());
+        for (int i = 0; i < events.size(); i++) {
+            assertEquals(0, resultMap.get(events.get(i)));
         }
     }
 }
