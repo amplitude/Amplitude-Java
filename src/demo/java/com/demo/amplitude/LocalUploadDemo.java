9d8b2ce9f126fd15c66534ed4f7858140606cd46
bohan-amplitude <bohan.li@amplitude.com>
2022-02-03 08:35:08 -0800

    apply formatter

--- src/demo/java/com/demo/amplitude/LocalUploadDemo.java
+++ src/demo/java/com/demo/amplitude/LocalUploadDemo.java
@@ -10,46 +10,50 @@ import java.util.concurrent.TimeUnit;
 
 public class LocalUploadDemo {
 
-    public static void main(String[] args) throws InterruptedException{
-        // Create and initialize Amplitude client
-        Amplitude client = Amplitude.getInstance();
-        client.init("");
+  public static void main(String[] args) throws InterruptedException {
+    // Create and initialize Amplitude client
+    Amplitude client = Amplitude.getInstance();
+    client.init("");
 
-        // use batch mode for higher throttling limits
-        client.useBatchMode(true);
+    // use batch mode for higher throttling limits
+    client.useBatchMode(true);
 
-        // this config can print debug info into console, including response body of each requests
-        client.setLogMode(AmplitudeLog.LogMode.DEBUG);
+    // this config can print debug info into console, including response body of each requests
+    client.setLogMode(AmplitudeLog.LogMode.DEBUG);
 
-        // for large amount of events to send, config a higher update threshold to upload more events in one request
-        // payload size limit for batch api is 20MB, max events per request is 2000
-        // https://developers.amplitude.com/docs/batch-event-upload-api#feature-comparison-between-httpapi-2httpapi--batch
-        client.setEventUploadThreshold(1500);
+    // for large amount of events to send, config a higher update threshold to upload more events in
+    // one request
+    // payload size limit for batch api is 20MB, max events per request is 2000
+    // https://developers.amplitude.com/docs/batch-event-upload-api#feature-comparison-between-httpapi-2httpapi--batch
+    client.setEventUploadThreshold(1500);
 
-        // config client to record throttled userId and deviceId. shouldWait(event) will return true if userid or deviceid was throttled
-        // can wait a short period of time and continue
-        client.setRecordThrottledId(true);
-        AmplitudeCallbacks callback = new AmplitudeCallbacks() {
-            @Override
-            public void onLogEventServerResponse(Event event, int status, String message) {
-                // call back functions here
-                System.out.println(event.eventType + " " + event.userId + " " + status + " " + message);
-            }
+    // config client to record throttled userId and deviceId. shouldWait(event) will return true if
+    // userid or deviceid was throttled
+    // can wait a short period of time and continue
+    client.setRecordThrottledId(true);
+    AmplitudeCallbacks callback =
+        new AmplitudeCallbacks() {
+          @Override
+          public void onLogEventServerResponse(Event event, int status, String message) {
+            // call back functions here
+            System.out.println(event.eventType + " " + event.userId + " " + status + " " + message);
+          }
         };
-        client.setCallbacks(callback);
-        for (int i = 0; i < 10000000; i++){
-            Event ampEvent = new Event("General"+ (i % 20), "Test_UserID_B" + (i % 5000));
-            while (client.shouldWait(ampEvent)) {
-                System.out.println("Client is busy. Waiting for log event " + ampEvent.eventType);
-                TimeUnit.SECONDS.sleep(60L);
-            }
-            ampEvent.userProperties = new JSONObject()
-                    .put("property1", "p"+i)
-                    .put("property2", "p"+i)
-                    .put("property3", "p"+i)
-                    .put("property4", "p"+i)
-                    .put("property5", "p"+i);
-            client.logEvent(ampEvent);
-        }
+    client.setCallbacks(callback);
+    for (int i = 0; i < 10000000; i++) {
+      Event ampEvent = new Event("General" + (i % 20), "Test_UserID_B" + (i % 5000));
+      while (client.shouldWait(ampEvent)) {
+        System.out.println("Client is busy. Waiting for log event " + ampEvent.eventType);
+        TimeUnit.SECONDS.sleep(60L);
+      }
+      ampEvent.userProperties =
+          new JSONObject()
+              .put("property1", "p" + i)
+              .put("property2", "p" + i)
+              .put("property3", "p" + i)
+              .put("property4", "p" + i)
+              .put("property5", "p" + i);
+      client.logEvent(ampEvent);
     }
+  }
 }
